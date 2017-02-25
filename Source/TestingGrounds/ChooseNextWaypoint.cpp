fd42b9bdc032dbda9e593b1c07af5cc761b04e44
RYAN\ryan3_000 <virtualhauntedhousedallas@gmail.com>
2017-02-24 20:37:07 -0600

    216 Deleting C++ Classes

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -3,19 +3,25 @@
 #include "TestingGrounds.h"
 #include "ChooseNextWaypoint.h"
 #include "AIController.h"
-#include "PatrollingGuard.h" // TODO remove coupling
+#include "PatrolRoute.h"
 #include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	//TODO protect against empty Patrol Points
-	
-	//Get the patrol points
-	auto AIController = OwnerComp.GetAIOwner();
-	auto ControlledPawn = AIController->GetPawn();
-	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
-	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
 
+	//Get the patrol route
+	
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+	if (!ensure(PatrolRoute)) { return EBTNodeResult::Failed; }
+
+	// Warn about empty patrol routes
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+		if (PatrolPoints.Num() == 0)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("A gaurd is missing patrol points"));
+			return EBTNodeResult::Failed;
+		}
 
 
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
--- Source/TestingGrounds/PatrolRoute.cpp
+++ Source/TestingGrounds/PatrolRoute.cpp
@@ -1,8 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TestingGrounds.h"
-#include "PatrollingGuard.h"
+#include "PatrolRoute.h"
 
 
-
-
+TArray<AActor*> UPatrolRoute::GetPatrolPoints() const
+{
+	return PatrolPoints;
+}
